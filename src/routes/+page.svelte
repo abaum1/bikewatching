55e67c3e93e0d4738f3cf36176c7ce6d1cd0615f
amelia-b <amelia_b@mit.edu>
2024-04-09 23:08:29 -0400

    filtering not quite working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,61 +1,252 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import mapboxgl from "mapbox-gl";
+	import "../../node_modules/mapbox-gl/dist/mapbox-gl.css";
+	import * as d3 from "d3";
+	mapboxgl.accessToken =
+		"pk.eyJ1IjoiYW1lbGlhLWIiLCJhIjoiY2x1cm9mZzB1MDNsczJqb2lldXBkd3Z0dyJ9.UFyVxsKyA6Z4EvtOWDCkng";
+
+	import { onMount } from "svelte";
+
+	let map;
+	let stations = [];
+	let bbTrips = [];
+	let mapViewChanged = 0;
+	let departures = [];
+	let arrivals = [];
+	let radiusScale;
+	let timeFilter = -1;
+	let departuresByMinute = Array.from({ length: 1440 }, () => []);
+	let arrivalsByMinute = Array.from({ length: 1440 }, () => []);
+
+	const TRIP_DATA_URL =
+		"https://vis-society.github.io/labs/8/data/bluebikes-traffic-2024-03.csv";
+
+	onMount(async () => {
+		// Wrap the map creation in a try-catch block to handle any potential errors
+		try {
+			map = new mapboxgl.Map({
+				container: "map", // Assuming you have a div element with id="map-container" in your HTML
+				zoom: 12,
+				style: "mapbox://styles/mapbox/light-v10",
+				center: [-71.08943706294963, 42.33920769686098],
+			});
+			await new Promise((resolve) => map.on("load", resolve));
+			map.addSource("boston_route", {
+				type: "geojson",
+				data: "https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D",
+			});
+			map.addLayer({
+				id: "boston-bike-lanes", // A name for our layer (up to you)
+				type: "line", // one of the supported layer types, e.g. line, circle, etc.
+				source: "boston_route", // The id we specified in `addSource()`
+				paint: {
+					// paint params, e.g. colors, thickness, etc.
+					"line-color": "green",
+				},
+			});
+			// cambridge bike lanes
+			map.addSource("cambridge_route", {
+				type: "geojson",
+				data: "https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson",
+			});
+			map.addLayer({
+				id: "cambridge-bike-lanes", // A name for our layer (up to you)
+				type: "line", // one of the supported layer types, e.g. line, circle, etc.
+				source: "cambridge_route", // The id we specified in `addSource()`
+				paint: {
+					// paint params, e.g. colors, thickness, etc.
+					"line-color": "green",
+				},
+			});
+			stations = await d3.csv(
+				"https://vis-society.github.io/labs/8/data/bluebikes-stations.csv",
+			);
+
+			bbTrips = await d3.csv(TRIP_DATA_URL).then((trips) => {
+				for (let trip of trips) {
+					trip.started_at = new Date(trip.started_at);
+					trip.ended_at = new Date(trip.ended_at);
+
+					let startMinutesIntoDay = minutesSinceMidnight(
+						trip.started_at,
+					);
+					let endMinutesIntoDay = minutesSinceMidnight(trip.ended_at);
+
+					departuresByMinute[startMinutesIntoDay].push(trip);
+					arrivalsByMinute[endMinutesIntoDay].push(trip);
+				}
+				return trips;
+			});
+
+			departures = d3.rollup(
+				bbTrips,
+				(v) => v.length,
+				(d) => d.start_station_id,
+			);
+			arrivals = d3.rollup(
+				bbTrips,
+				(v) => v.length,
+				(d) => d.end_station_id,
+			);
+			stations = stations.map((station) => {
+				let id = station.Number;
+				station.arrivals = arrivals.get(id) ?? 0;
+				station.departures = departures.get(id) ?? 0;
+				station.totalTraffic = station.arrivals + station.departures;
+				return station;
+			});
+
+			console.log("stations", stations);
+		} catch (error) {
+			console.error("Error loading map:", error);
+		}
+	});
+
+	function getCoords(station) {
+		let point = new mapboxgl.LngLat(+station.Long, +station.Lat);
+		let { x, y } = map.project(point);
+		return { cx: x, cy: y };
+	}
+
+	$: radiusScale = d3
+		.scaleSqrt()
+		.domain([0, d3.max(stations, (d) => d.totalTraffic)])
+		.range([0, 25]);
+
+	$: map?.on("move", (evt) => mapViewChanged++);
+	$: timeFilterLabel =
+		timeFilter === -1
+			? "(any time)"
+			: new Date(0, 0, 1, 0, timeFilter).toLocaleString("en", {
+					timeStyle: "short",
+				});
+
+	function minutesSinceMidnight(date) {
+		return date.getHours() * 60 + date.getMinutes();
+	}
+
+	function filterByMinute(tripsByMinute, minute) {
+		// Normalize both to the [0, 1439] range
+		// % is the remainder operator: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder
+		let minMinute = (minute - 60 + 1440) % 1440;
+		let maxMinute = minute + (60 % 1440);
+
+		if (minMinute > maxMinute) {
+			let beforeMidnight = tripsByMinute.slice(minMinute);
+			let afterMidnight = tripsByMinute.slice(0, maxMinute);
+			return beforeMidnight.concat(afterMidnight).flat();
+		} else {
+			return tripsByMinute.slice(minMinute, maxMinute).flat();
+		}
+	}
+	$: filteredDepartures = filterByMinute(departuresByMinute, timeFilter);
+	$: filteredArrivals = filterByMinute(arrivalsByMinute, timeFilter);
+
+	$: filteredDeparturesAggregate = d3.rollup(
+		filteredDepartures,
+		(v) => v.length,
+		(d) => d.start_station_id,
+	);
+	$: filteredArrivalsAggregate = d3.rollup(
+		filteredArrivals,
+		(v) => v.length,
+		(d) => d.end_station_id,
+	);
+	console.log("timeFilterLabel", timeFilterLabel);
+	console.log("filteredArrivalsAggregate", filteredArrivalsAggregate);
+	console.log("arrivalsByMinute", arrivalsByMinute);
+	console.log("filteredArrivals", filteredArrivals);
+
+	$: stations =
+		timeFilter === -1
+			? stations
+			: stations.map((station) => {
+					station = { ...station }; // Clone to avoid mutating the original
+					let id = station.Number;
+					station.departures = filteredDepartures.get(id) ?? 0;
+					station.arrivals = filteredArrivals.get(id) ?? 0;
+					station.totalTraffic =
+						station.departures + station.arrivals;
+					return station;
+				});
+
+	console.log("timeFilter", timeFilter);
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<link rel="icon" type="image/png" href="images/bike.svg" />
+	<title>Bikewatching</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+<h1>Bikewatching</h1>
+<header class="timeselector">
+	<label>Filter by time of day:</label>
+	<input type="range" min="-1" max="1440" bind:value={timeFilter} />
+	{#if timeFilter == -1}
+		<!-- <time>{timeFilterLabel}</time> -->
+		<em>(any time)</em>
+	{:else}
+		<time>
+			{timeFilterLabel}
+		</time>
+	{/if}
+</header>
+<p>
+	This is an immersive visualization map of bike traffic in the Boston area.
+</p>
+<div id="map">
+	{#key mapViewChanged}
+		<svg>
+			{#each stations as station}
+				<circle
+					{...getCoords(station)}
+					r={radiusScale(station.totalTraffic)}
+					fill="steelblue"
+				>
+					<title
+						>{station.totalTraffic} trips ({station.departures} departures,
+						{station.arrivals} arrivals)</title
+					>
+				</circle>
+			{/each}
+		</svg>
+	{/key}
+</div>
 
 <style>
 	@import url("$lib/global.css");
-
-	/* section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+	#map {
+		flex: 1;
+		background-color: yellowgreen;
 	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
+	#map svg {
 		width: 100%;
 		height: 100%;
-		top: 0;
+		position: absolute;
+		z-index: 1;
+		pointer-events: none;
+
+		circle {
+			pointer-events: auto;
+			fill-opacity: 60%;
+			stroke: white;
+		}
+	}
+	.timeselector {
+		display: flex;
+		gap: 1em;
+		align-items: center;
+		label {
+			margin-left: auto;
+		}
+	}
+
+	em {
+		color: #888; /* Lighter color */
+		font-style: italic; /* Italic font */
 		display: block;
-	} */
+	}
+	time {
+		display: block;
+	}
 </style>
